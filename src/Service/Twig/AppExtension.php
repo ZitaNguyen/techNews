6051fdb889d2fab72db6cc408b46289be324d616
Zita Nguyen <zitanguyen84@gmail.com>
2019-04-19 12:44:17 +0200

    travail 19/04

---
+++ src/Service/Twig/AppExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Service\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('summarize', function( $contenu ) {
+
+                // Suppression des balises HTML
+                $string = strip_tags($contenu);
+
+                // Si mon $string est supérieur à 150, je continue
+                if (strlen($string) > 150) {
+                    // je coupe ma chaine à 150
+                    $stringCut = substr($string, 0, 150);
+                    // je m'assure de ne pas couper un mot
+                    // en recherchant la position du dernier espace
+                    $string = substr($stringCut, 0, strrpos($stringCut, ' '));
+                }
+
+                return $string . '...';
+
+            }, array( 'is_safe' => array( 'html' ) ) )
+        ];
+    }
+
+}
